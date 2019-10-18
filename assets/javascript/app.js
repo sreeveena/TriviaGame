6a12e47c9119f9bfb6f9e80d4b740ef8e0a4f770
sreeveena <sreeveena21@gmail.com>
2019-10-17 18:20:49 -0700

    filled trivia questions.

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+
+    var myQuestions = [
+        {
+            question: "In A Bug's Life, the bugs that Flik mistakes for warriors actually worked where?",
+            answers: ['The theater', 'The circus', 'The mall', 'The farm'],
+            correctAnswer: 'b'
+        },
+        {
+            question: "In Toy Story 2, what's the name of the 1950s puppet show in which Woody was the star?",
+            answers: ['The Woody hour', 'Cowboy hour', 'Woody and the gang', 'Woody roundup'],
+            correctAnswer: 'd'
+        },
+        {
+            question: "When Monsters, Inc. opens up, who's the top scarer at the company?",
+            answers: ['Mike', 'Sulley', 'Randall', 'Frank McCay'],
+            correctAnswer: 'b'
+        },
+        {
+            question: "In The Incredibles, what's Frozone's actual first name?",
+            answers: ['Lucas', 'Lewis', 'Lucius', 'Loomis'],
+            correctAnswer: 'c'
+        },
+        {
+            question: "In Cars, the Piston Cup is being held at what venue?",
+            answers: ['Los Angeles International Speedway', 'Daytona International Speedway', 'Texas Motor Speedway', 'Talladega Superspeedway'],
+            correctAnswer: 'a'
+        },
+        {
+            question: "In WALL-E, what does EVE visit Earth in search of?",
+            answers: ['Oil', 'Plant life', 'Friendship', 'Anything of value'],
+            correctAnswer: 'b'
+        },
+        {
+            question: "In Up, where is Paradise Falls located?",
+            answers: ['Costa Rica', 'Chile', 'Guatemala', 'Venezuela'],
+            correctAnswer: 'd'
+        },
+        {
+            question: "Which character doesn't make an appearance in Toy Story 3?",
+            answers: ['Slinky', 'Bo Peep', 'Rex', 'Mrs.Potato head'],
+            correctAnswer: 'b'
+        },
+        {
+            question: "In Cars 2, where is the first race of the World Grand Prix held?",
+            answers: ['London', 'Hong Kong', 'Tokyo', 'Paris'],
+            correctAnswer: 'c'
+        },
+        {
+            question: "What's the name of Monsters University's rival school in Monsters University?",
+            answers: ['Fear University', 'Scare Tech', 'Scare State', 'Fear Tech'],
+            correctAnswer: 'd'
+        },
+    ];
+
